c0aed907d8dc1cc8b9e18c14515ac33247a30eb4
Chun Chen <ramichen@tencent.com>
2015-11-11 14:58:20 +0800

    Add emulated /proc/diskstats
    Signed-off-by: Chun Chen <ramichen@tencent.com>

--- fs/dir.go
+++ fs/dir.go
@@ -33,6 +33,12 @@ func (d Dir) Lookup(ctx context.Context, name string) (fusefs.Node, error) {
 			return nil, fuse.ENODATA
 		}
 		return NewMemInfoFile(filepath.Join(memMountPoint, d.cgroupdir)), nil
+	} else if name == "diskstats" {
+		blkioMountPoint, err := cgroups.FindCgroupMountpoint("blkio")
+		if err != nil {
+			return nil, fuse.ENODATA
+		}
+		return NewDiskStatsFile(filepath.Join(blkioMountPoint, d.cgroupdir)), nil
 	}
 	return nil, fuse.ENOENT
 }
@@ -41,5 +47,6 @@ func (Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 	return []fuse.Dirent{
 		{Inode: 2, Name: "hello", Type: fuse.DT_File},
 		{Inode: 3, Name: "meminfo", Type: fuse.DT_File},
+		{Inode: 4, Name: "diskstats", Type: fuse.DT_File},
 	}, nil
 }
---
+++ fs/diskstats.go
@@ -0,0 +1,114 @@
+package fs
+
+import (
+	"bazil.org/fuse"
+	_ "bazil.org/fuse/fs/fstestutil"
+
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/opencontainers/runc/libcontainer/cgroups"
+	"github.com/opencontainers/runc/libcontainer/cgroups/fs"
+	"golang.org/x/net/context"
+	"io/ioutil"
+)
+
+// File implements both Node and Handle for the hello file.
+type DiskStatsFile struct {
+	cgroupdir  string
+	blkioGroup fs.BlkioGroup
+}
+
+func NewDiskStatsFile(cgroupdir string) DiskStatsFile {
+	return DiskStatsFile{cgroupdir, fs.BlkioGroup{}}
+}
+
+//https://www.kernel.org/doc/Documentation/cgroups/blkio-controller.txt
+//https://www.kernel.org/doc/Documentation/iostats.txt
+
+//blkio.throttle.io_service_bytes
+//8:0 Read 0
+//8:0 Write 774144
+//8:0 Sync 0
+//8:0 Async 774144
+//8:0 Total 774144
+//Total 774144
+//blkio.throttle.io_serviced
+//8:0 Read 0
+//8:0 Write 189
+//8:0 Sync 0
+//8:0 Async 189
+//8:0 Total 189
+//Total 189
+
+//docker create  --device /dev/fuse --cap-add SYS_ADMIN -v /tmp/cgroupfs/meminfo:/proc/meminfo ubuntu bash -c "while true; do echo 213 > /tmp/log; sleep 1; cat /tmp/log; done"
+
+type BlkioOp string
+
+const (
+	Read  BlkioOp = "Read"
+	Write BlkioOp = "Write"
+	Sync  BlkioOp = "Sync"
+	Async BlkioOp = "Async"
+	Total BlkioOp = "Total"
+)
+
+func (ds DiskStatsFile) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 2
+	a.Mode = 0444
+	data, _ := ds.ReadAll(ctx)
+	a.Size = uint64(len(data))
+	return nil
+}
+
+func (ds DiskStatsFile) ReadAll(ctx context.Context) ([]byte, error) {
+	stats := cgroups.NewStats()
+	ds.blkioGroup.GetStats(ds.cgroupdir, stats)
+	blkioStats := stats.BlkioStats
+
+	diskStats, _ := ioutil.ReadFile("/proc/diskstats")
+	sc := bufio.NewScanner(bytes.NewReader(diskStats))
+	var (
+		major, minor                  uint64
+		dev                           string
+		read, readMerged              uint64
+		readSector, readMiliSec       uint64
+		write, writeMerged            uint64
+		writeSector, writeMiliSec     uint64
+		ios, ioMiliSec, ioWaitMiliSec uint64
+	)
+	buf := bytes.NewBuffer(make([]byte, 0))
+	for sc.Scan() {
+		line := sc.Text()
+		if n, err := fmt.Sscanf(line, "%d %d %s", &major, &minor, &dev); n == 3 && err == nil {
+			logrus.Debugf("Get blkio stats %d %d %s", major, minor, dev)
+			read = getBlkioStats(blkioStats.IoServicedRecursive, major, minor, Read)
+			readMerged = getBlkioStats(blkioStats.IoMergedRecursive, major, minor, Read)
+			readSector = getBlkioStats(blkioStats.IoServiceBytesRecursive, major, minor, Read) / 512
+			readMiliSec = (getBlkioStats(blkioStats.IoServiceTimeRecursive, major, minor, Read) + getBlkioStats(blkioStats.IoWaitTimeRecursive, major, minor, Read)) / 1000000
+			write = getBlkioStats(blkioStats.IoServicedRecursive, major, minor, Write)
+			writeMerged = getBlkioStats(blkioStats.IoMergedRecursive, major, minor, Write)
+			writeSector = getBlkioStats(blkioStats.IoServiceBytesRecursive, major, minor, Write) / 512
+			writeMiliSec = (getBlkioStats(blkioStats.IoServiceTimeRecursive, major, minor, Write) + getBlkioStats(blkioStats.IoWaitTimeRecursive, major, minor, Write)) / 1000000
+			ios = getBlkioStats(blkioStats.IoQueuedRecursive, major, minor, Total)
+			ioMiliSec = getBlkioStats(blkioStats.IoTimeRecursive, major, minor, Total) / 1000000
+			ioWaitMiliSec = getBlkioStats(blkioStats.IoWaitTimeRecursive, major, minor, Total) / 1000000
+			if read != 0 || readMerged != 0 || readSector != 0 || readMiliSec != 0 || write != 0 || writeMerged != 0 || writeSector != 0 || writeMiliSec != 0 || ios != 0 || ioMiliSec != 0 || ioWaitMiliSec != 0 {
+				fmt.Fprintf(buf, "%d       %d %s %d %d %d %d %d %d %d %d %d %d %d\n",
+					major, minor, dev, read, readMerged, readSector, readMiliSec,
+					write, writeMerged, writeSector, writeMiliSec, ios, ioMiliSec, ioWaitMiliSec)
+			}
+		}
+	}
+	return buf.Bytes(), nil
+}
+
+func getBlkioStats(entries []cgroups.BlkioStatEntry, major, minor uint64, op BlkioOp) uint64 {
+	for _, entry := range entries {
+		if entry.Major == major && entry.Minor == minor && entry.Op == string(op) {
+			return entry.Value
+		}
+	}
+	return 0
+}
